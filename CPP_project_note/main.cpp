925ed153fc2751b973c7a8f9a535eefb5c7aefe5
NpsCause <57292390+noh1216@users.noreply.github.com>
2022-04-29 01:23:13 +0900

    Initial Commit

---
+++ CPP_project_note/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPP_project_note
+//
+//  Created by NpsCause on 2022/04/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
